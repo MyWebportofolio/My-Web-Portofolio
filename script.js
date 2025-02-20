472e6c30ad1f2f5b2422898a0d53c49c176fc6dc
MyWebportofolio <lgrauwin@gmail.com>
2025-02-20 10:27:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("hamburger-menu").style.width = "60%";
+document.getElementById("hamburger-menu").style.display="block";    
+}
+
+function closeNav() {
+    document.getElementById("hamburger-menu").style.display = "none";
+}
