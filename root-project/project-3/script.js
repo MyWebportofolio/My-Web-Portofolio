9b5f9669892a332f7f4ee17072ac4617ddbbb905
MyWebportofolio <lgrauwin@gmail.com>
2025-02-24 15:29:02 +0100

    Update script.js

--- root-project/project-3/script.js
+++ root-project/project-3/script.js
@@ -1,1 +1,8 @@
+var catPic = document.getElementById("cat-pic");
+var onCatClick = function(e) {
+    var stashePic = document.getElementById("mustache-pic");  
+    stashePic.style.top = e.clientY+"px";
+    stashePic.style.left = e.clientX+"px";
+};
+catPic.addEventListener("click", onCatClick);
 
