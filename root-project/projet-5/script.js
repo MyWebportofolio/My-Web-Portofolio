a8900253de09952b2ffe35688e105775619ea0eb
MyWebportofolio <lgrauwin@gmail.com>
2025-02-25 16:50:08 +0100

    Update script.js

--- root-project/projet-5/script.js
+++ root-project/projet-5/script.js
@@ -1,1 +1,5 @@
+$("h1").text("Butterflies");
+    $("p").html("<strong>Butterflies</strong> can have vivid colors");
+    $("img").attr("src",'https://www.kasandbox.org/programming-images/animals/butterfly_monarch.png')
+    $("h1").css("color","green")
 
