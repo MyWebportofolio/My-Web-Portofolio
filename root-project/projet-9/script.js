51a23c6e80f5c60eb15111b6cc31c45bfb8bdbe6
MyWebportofolio <lgrauwin@gmail.com>
2026-09-10 19:14:40 +0200

    Update script.js
    adding catch statement to try to locate error

--- root-project/projet-9/script.js
+++ root-project/projet-9/script.js
@@ -1,4 +1,6 @@
 function parler(lettre) {
   const audio = new Audio('a.mp3');
-  audio.play();
+  audio.play().catch(error => {
+    console.warn('Audio playback prevented by browser:', error);
+  });
 }
