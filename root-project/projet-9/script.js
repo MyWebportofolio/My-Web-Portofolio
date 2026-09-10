8a27de2e4b4a0d0afc1a1ee288ee4f87ad9c56af
MyWebportofolio <lgrauwin@gmail.com>
2026-09-10 22:00:54 +0200

    Update script.js

--- root-project/projet-9/script.js
+++ root-project/projet-9/script.js
@@ -2,7 +2,5 @@ function parler(lettre) {
   // Backticks are key here (typically found on the key under Esc or key 7 on AZERTY keyboards)
   const audio = new Audio(`sons/${lettre}.mp3`);
   
-  audio.play().catch(error => {
-    console.error('Playback error:', error);
-  });
+  audio.play();
 }
